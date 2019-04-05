242918c677b45f752799e8e0a72db8d7b54551e9
Joe Sitton <sittonjoe@gmail.com>
2019-04-05 15:04:25 -0400

    +pics, -text and avatar

--- photography.js
+++ photography.js
@@ -2,18 +2,17 @@ var vm = new Vue({
     el: '#photos',
     data: {
         photos: [
+            {url: "assets/photos/bokeh_polaroid.jpg"},
+            {url: "assets/photos/lunareclipse.jpg"},
+            {url: "assets/photos/2019-01-06_14-16-44_UTC.jpg"},
             {url: "assets/photos/2018-12-24_21-35-30_UTC.jpg"},
             {url: "assets/photos/2018-12-24_21-37-28_UTC.jpg"},
             {url: "assets/photos/2018-12-24_21-37-40_UTC.jpg"},
-            {url: "assets/photos/2018-12-25_16-15-52_UTC.jpg"},
             {url: "assets/photos/2018-12-26_17-39-41_UTC.jpg"},
             {url: "assets/photos/2018-12-26_17-41-17_UTC.jpg"},
             {url: "assets/photos/2018-12-26_17-54-22_UTC.jpg"},
             {url: "assets/photos/2018-12-27_15-35-38_UTC.jpg"},
-            {url: "assets/photos/2018-12-29_21-25-58_UTC.jpg"},
             {url: "assets/photos/2019-01-02_18-34-38_UTC.jpg"},
-            {url: "assets/photos/2019-01-06_14-16-44_UTC.jpg"},
-            {url: "assets/photos/2019-01-06_17-41-10_UTC.jpg"},
         ]
     }
 })
